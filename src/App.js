a92c55477195871dec0fcb9016f25b8cf0a1157f
M-Abdullah-Bhatti <muhammadabdullahimdad@gmail.com>
2023-06-09 17:27:26 +0500

    user wallet address getted

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { Container } from "react-bootstrap";
 import HomeScreen from "./screens/HomeScreen";
-import {HashRouter as Router, Route, Routes} from "react-router-dom"
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
@@ -18,35 +18,53 @@ import ProductListScreen from "./screens/ProductListScreen";
 import ProductEditScreen from "./screens/ProductEditScreen";
 import OrderListScreen from "./screens/OrderListScreen";
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function App() {
   return (
     <Router className="App">
       <Header />
-      <main className="py-3" >
-      <Container>
-    <Routes>
-        <Route path="/" exact element={<HomeScreen/>} />
-        <Route path="/login" exact element={<LoginScreen/>} />
-        <Route path="/register" exact element={<RegisterScreen/>} />
-        <Route path="/profile" exact element={<ProfileScreen/>} />
-        <Route path="/login/shipping" exact element={<ShippingScreen/>} />
-        <Route path="/placeorder" exact element={<PlaceOrderScreen/>} />
-        <Route path="/payment" exact element={<PaymentScreen/>} />
-        <Route path="/order/:id" exact element={<OrderScreen/>} />
-        <Route exact path="/product/:id" element={<ProductScreen/>} />
-        <Route exact path="/cart" element={<CartScreen/>} />
-        <Route exact path="/cart/:id" element={<CartScreen/>} />
+      <main className="py-3">
+        <Container>
+          <ToastContainer position="top-center" autoClose={3000} />
+          <Routes>
+            <Route path="/" exact element={<HomeScreen />} />
+            <Route path="/login" exact element={<LoginScreen />} />
+            <Route path="/register" exact element={<RegisterScreen />} />
+            <Route path="/profile" exact element={<ProfileScreen />} />
+            <Route path="/login/shipping" exact element={<ShippingScreen />} />
+            <Route path="/placeorder" exact element={<PlaceOrderScreen />} />
+            <Route path="/payment" exact element={<PaymentScreen />} />
+            <Route path="/order/:id" exact element={<OrderScreen />} />
+            <Route exact path="/product/:id" element={<ProductScreen />} />
+            <Route exact path="/cart" element={<CartScreen />} />
+            <Route exact path="/cart/:id" element={<CartScreen />} />
 
-        <Route exact path="/admin/product/:id/edit" element={<ProductEditScreen/>} />
-        <Route exact path="/admin/userlist" element={<UserListScreen/>} />
-        <Route exact path="/admin/user/:id/edit" element={<UserEditScreen/>} />
+            <Route
+              exact
+              path="/admin/product/:id/edit"
+              element={<ProductEditScreen />}
+            />
+            <Route exact path="/admin/userlist" element={<UserListScreen />} />
+            <Route
+              exact
+              path="/admin/user/:id/edit"
+              element={<UserEditScreen />}
+            />
 
-        <Route exact path="/admin/productlist" element={<ProductListScreen/>} />
-        <Route exact path="/admin/orderlist" element={<OrderListScreen/>} />
-
-        </Routes>
-      </Container>
-
+            <Route
+              exact
+              path="/admin/productlist"
+              element={<ProductListScreen />}
+            />
+            <Route
+              exact
+              path="/admin/orderlist"
+              element={<OrderListScreen />}
+            />
+          </Routes>
+        </Container>
       </main>
       <Footer />
     </Router>
